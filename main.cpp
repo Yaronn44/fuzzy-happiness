639d92b9414360f7b820a89bc60f1e09dde237b1
Yaronn44 <ivan-44@hotmail.fr>
2016-10-12 15:08:24 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,30 +10,30 @@ using namespace std;
 int main(int argc, char const *argv[]){
 
 	Humain matheux;
-	Humain keupon;
+	Humain papy;
 
-	Arme couteau("sa bite", 1000, 45);
+	Arme couteau("un poireaux", 1000, 45);
 
 	matheux.prendre_objet(couteau);
 	matheux.set_arme(couteau);
-	cout << "Matheux n'a pour arme que " << matheux.get_arme().get_nom() << endl;
-	matheux.attaquer(keupon);
+	cout << "Matheux n'a pour arme qu' " << matheux.get_arme().get_nom() << endl;
+	matheux.attaquer(papy);
 
-	cout << "Matheux frappe Keupon dans la bouche avec " << matheux.get_arme().get_nom() << ". Il reste " << keupon.get_pdv() << " point de vie à Keupon" << endl;
+	cout << "Matheux frappe Papy dans la machoire avec " << matheux.get_arme().get_nom() << ". Il reste " << papy.get_pdv() << " point de vie à Papy" << endl;
 
 	Aliment merde("Merde", 1, 5);
 
-	keupon.prendre_objet(merde);
-	keupon.manger(merde);
+	papy.prendre_objet(merde);
+	papy.manger(merde);
 
-	cout << "Keupon mange de la merde, cela lui rend " << merde.get_soin() << " pdv. Il a maintenant " << keupon.get_pdv() << " pdv." << endl;
+	cout << "Papy mange de la merde, cela lui rend " << merde.get_soin() << " pdv. Il a maintenant " << papy.get_pdv() << " pdv." << endl;
 
-	Aliment tiramisu("Merde survitaminée", 1, 80);
+	Aliment tiramisu("Gateau de Mamy", 1, 80);
 
-	keupon.prendre_objet(tiramisu);
-	keupon.manger(tiramisu);
+	papy.prendre_objet(tiramisu);
+	papy.manger(tiramisu);
 
-	cout << "Keupon mange un tiramisu, cela lui rend " << tiramisu.get_soin() << " pdv. Il a maintenant " << keupon.get_pdv() << " pdv." << endl;
+	cout << "Papy mange un tiramisu, cela lui rend " << tiramisu.get_soin() << " pdv. Il a maintenant " << papy.get_pdv() << " pdv." << endl;
 
 	return 0;
 }
